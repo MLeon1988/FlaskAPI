a3626408eaa8ea91f063781646074ea57cd6a833
new owner <newowner@news-MacBook-Air.local>
2022-08-08 15:26:36 -0400

    updated-crud

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, jsonify, request
 from peewee import *
 from playhouse.shortcuts import model_to_dict, dict_to_model
 
-db = PostgresqlDatabase('people', user='newowner', password='', host='localhost', port=5432)
+db = PostgresqlDatabase('people', user='newowner',
+                        password='', host='localhost', port=5432)
+
 
 class BaseModel(Model):
-  class Meta:
-    database = db
+    class Meta:
+        database = db
+
 
 class Person(BaseModel):
-  name = CharField()
-  age = IntegerField()
+    name = CharField()
+    age = IntegerField()
+
 
 db.connect()
 
@@ -22,27 +26,33 @@ Person(name='Mack', age=2000).save()
 
 app = Flask(__name__)
 
+
 @app.route('/person/', methods=['GET', 'POST'])
 @app.route('/person/<id>', methods=['GET', 'PUT', 'DELETE'])
 def endpoint(id=None):
-  if request.method == 'GET':
-    if id:
-        return jsonify(model_to_dict(Person.get(Person.id == id)))
-    else:
-        people_list = []
-        for person in Person.select():
-            people_list.append(model_to_dict(person))
-        return jsonify(people_list)
+    if request.method == 'GET':
+        if id:
+            return jsonify(model_to_dict(Person.get(Person.id == id)))
+        else:
+            people_list = []
+            for person in Person.select():
+                people_list.append(model_to_dict(person))
+            return jsonify(people_list)
 
-  if request.method == 'PUT':
-    return 'PUT request'
+    if request.method == 'PUT':
+        if request.method == 'PUT':
+            body = request.get_json()
+            Person.update(body).where(Person.id == id).execute()
+            return "Person " + str(id) + " has been updated."
 
-  if request.method == 'POST':
-    new_person = dict_to_model(Person, request.get_json())
-    new_person.save()
-    return jsonify({"success": True})
+    if request.method == 'POST':
+        new_person = dict_to_model(Person, request.get_json())
+        new_person.save()
+        return jsonify({"success": True})
 
-  if request.method == 'DELETE':
-    return 'DELETE request'
+    if request.method == 'DELETE':
+        People.delete().where(People.id == id).execute()
+        return "People " + str(id) + " deleted."
+
 
 app.run(debug=True, port=5000)
